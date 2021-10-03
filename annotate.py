12c0645c64a02cbb11e71effadde12506a9970fe
Jiayi Tang <jiayitang10@gmail.com>
2021-10-02 18:11:58 -0700

    sync to pc

--- annotate.py
+++ annotate.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from collections import defaultdict
+import requests
 
 class Transcript:
     def __init__(self, html) -> None:
@@ -12,8 +13,20 @@ class Transcript:
             courses[courseName[0]].append(courseName[1])
         return courses
 
-# def getCourseNames(subjectCode, courseList):
-
+def getCourseNames(subjectCode, courses):
+    url = f"http://www.calendar.ubc.ca/vancouver/courses.cfm?page=code&code={subjectCode}"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.content, features="lxml")
+    courseListings = [x.get_text() for x in soup.find_all("dt")]
+    
+    for course in courses:
+        matchingCourses = [x.split(")")[1].lstrip() for x in courseListings if course in x]
+        assert len(matchingCourses) == 1
+        print(matchingCourses)
+    
 
 tr = Transcript(open("transcript.html", "r"))
-print(tr.getCourses())
+courseDict = tr.getCourses()
+
+for subjectCode, courses in courseDict.items():
+    getCourseNames(subjectCode, courses)
