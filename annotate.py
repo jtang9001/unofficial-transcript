ac3f287a725c77e1d6b58f7b4a542e998ca0010a
Jiayi Tang <jiayitang10@gmail.com>
2021-10-02 20:14:02 -0700

    works! table was filled

--- annotate.py
+++ annotate.py
@@ -5,28 +5,41 @@ import requests
 class Transcript:
     def __init__(self, html) -> None:
         self.soup = BeautifulSoup(html, features = "lxml")
+        self.getCourses()
+        self.getCourseNames()
 
     def getCourses(self):
-        courses = defaultdict(list)
+        self.coursesSorted = defaultdict(list)
         for row in self.soup.find_all("tr", class_ = "listRow"):
             courseName = row.find("td").get_text().split()
-            courses[courseName[0]].append(courseName[1])
-        return courses
+            self.coursesSorted[courseName[0]].append(courseName[1])
+        return self.coursesSorted
 
-def getCourseNames(subjectCode, courses):
-    url = f"http://www.calendar.ubc.ca/vancouver/courses.cfm?page=code&code={subjectCode}"
-    r = requests.get(url)
-    soup = BeautifulSoup(r.content, features="lxml")
-    courseListings = [x.get_text() for x in soup.find_all("dt")]
-    
-    for course in courses:
-        matchingCourses = [x.split(")")[1].lstrip() for x in courseListings if course in x]
-        assert len(matchingCourses) == 1
-        print(matchingCourses)
-    
+    def getCourseNames(self):
+        self.courses = {}        
+        for subjectCode, courseNumbers in self.coursesSorted.items():
+            url = f"http://www.calendar.ubc.ca/vancouver/courses.cfm?page=code&code={subjectCode}"
+            r = requests.get(url)
+            soup = BeautifulSoup(r.content, features="lxml")
+            courseListings = [x.get_text() for x in soup.find_all("dt")]
 
-tr = Transcript(open("transcript.html", "r"))
-courseDict = tr.getCourses()
+            for courseNumber in courseNumbers:
+                matchingCourses = [x.split(")")[1].lstrip() for x in courseListings if courseNumber in x]
+                assert len(matchingCourses) == 1
+                self.courses[f"{subjectCode}{courseNumber}"] = matchingCourses[0]
+        return self.courses
 
-for subjectCode, courses in courseDict.items():
-    getCourseNames(subjectCode, courses)
+    def fillCourseNames(self):
+        for courseCode, courseTitle in self.courses.items():
+            tag = self.soup.find("td", id=courseCode)
+            print(tag)
+            print(courseCode)
+            print(courseTitle)
+            tag.string = courseTitle
+        with open("transcript_filled.html", "w", encoding="utf-8") as f:
+            f.write(str(self.soup))
+        
+
+tr = Transcript(open("transcript.html", "r", encoding="utf-8"))
+print(tr.getCourseNames())
+tr.fillCourseNames()
